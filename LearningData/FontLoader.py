12eae9f446474d3b483bf81042d8da48323c5af3
Hubert <hubertwe@interia.pl>
2014-11-16 19:13:50 +0100

    Resize each letter bitmap by adding empty rows and cells.

--- LearningData/FontLoader.py
+++ LearningData/FontLoader.py
@@ -6,22 +6,32 @@ import string
 class FontLoader:
 
     def __init__(self, fontPath, arraySize):
-        self.fontPath = fontPath
-        self.arraySize = arraySize
+        self._fontPath = fontPath
+        self._arraySize = arraySize
 
     def getLetters(self):
-        letters = list();
-        font = ImageFont.truetype(self.fontPath, self.arraySize)
+        letters = dict();
+        font = ImageFont.truetype(self._fontPath, self._arraySize)
         '''Just uppercase for now'''
         for char in string.uppercase:
             im = Image.Image()._new(font.getmask(char))
             binaryLetter = numpy.where(numpy.array(im)>0, 1, 0)
-            letters.append(binaryLetter)
+            letters[char] = self._resize(binaryLetter)
         return letters
 
+    def _resize(self, letterArray):
+        letter = letterArray.copy()
+        widthDiff = self._arraySize - len(letter)
+        heightDiff = self._arraySize - len(letter[0])
+        if heightDiff > 0:
+            for i in range(0, heightDiff):
+                letter = numpy.hstack((letter, [[0]]*len(letter)))
+        if widthDiff > 0:
+            for i in range(0, widthDiff):
+                letter = numpy.vstack((letter, [0]*len(letter[0])))
+        return letter
+
+
 if __name__ == '__main__':
-    fl = FontLoader('pixelmix.ttf', 8)
+    fl = FontLoader('fonts/pixelmix.ttf', 16)
     letters = fl.getLetters()
-    for letter in letters:
-        print letter
-        print '\n'
